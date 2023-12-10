2c66a0784add207dbaed5b6716813bf32889dcf1
alimirza8 <alimirza.dev@gmail.com>
2023-12-10 23:31:17 +0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Marinate_The_Restaurant
+ */
+
+?>
+
+<?php /*
+<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'marinate-the-restaurant' ) ); ?>">
+				<?php
+				printf( esc_html__( 'Proudly powered by %s', 'marinate-the-restaurant' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'marinate-the-restaurant' ), 'marinate-the-restaurant', '<a href="http://underscores.me/">Ali Mirza</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+*/ ?>
+<footer>
+  <div class="frame black"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+        <div class="footer_wp">
+          <i class="icon_pin_alt"></i>
+          <h3>Address</h3>
+          <p>Chenab School Road, Servis Industries, <br>Gujrat - Pakistan</p>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+        <div class="footer_wp">
+          <i class="icon_tag_alt"></i>
+          <h3>Reservations</h3>
+          <p><a href="tel:+920533724692">+92 053 3724692</a><br><a href="#0">reservations@marinatetherestaurant.com</a></p>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+        <div class="footer_wp">
+          <i class="icon_clock_alt"></i>
+          <h3>Opening Hours</h3>
+          <ul>
+            <li>Mon - Sat: 11am - 11:30pm</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+        <h3>Keep in touch</h3>
+        <div id="newsletter">
+          <div id="message-newsletter"></div>
+          <form method="post" action="phpmailer/newsletter_template_email.php" name="newsletter_form" id="newsletter_form">
+            <div class="form-group">
+              <input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+              <button type="submit" id="submit-newsletter"><i class="arrow_carrot-right"></i></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- /row-->
+    <hr>
+    <div class="row">
+      <div class="col-sm-5">
+        <p class="copy">© Marinate The Restaurant - All rights reserved</p>
+      </div>
+      <div class="col-sm-7">
+        <div class="follow_us">
+          <ul>
+            <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<?php asset_path( 'img/twitter_icon.svg', true ); ?>" alt="" class="lazy"></a></li>
+            <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<?php asset_path( 'img/facebook_icon.svg', true ); ?>" alt="" class="lazy"></a></li>
+            <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<?php asset_path( 'img/instagram_icon.svg', true ); ?>" alt="" class="lazy"></a></li>
+            <li><a href="#0"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<?php asset_path( 'img/youtube_icon.svg', true ); ?>" alt="" class="lazy"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <p class="text-center"></p>
+  </div>
+</footer>
+<!--/footer-->
+
+<div id="toTop"></div><!-- Back to top button -->
+
+<!-- Modal terms -->
+<div class="modal fade" id="terms-txt" tabindex="-1" role="dialog" aria-labelledby="termsLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="termsLabel">Terms and conditions</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Lorem ipsum dolor sit amet, in porro albucius qui, in <strong>nec quod novum accumsan</strong>, mei ludus tamquam dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus, pro ne quod dicunt sensibus.</p>
+        <p>Lorem ipsum dolor sit amet, in porro albucius qui, in nec quod novum accumsan, mei ludus tamquam dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus, pro ne quod dicunt sensibus. Lorem ipsum dolor sit amet, <strong>in porro albucius qui</strong>, in nec quod novum accumsan, mei ludus tamquam dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus, pro ne quod dicunt sensibus.</p>
+        <p>Lorem ipsum dolor sit amet, in porro albucius qui, in nec quod novum accumsan, mei ludus tamquam dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus, pro ne quod dicunt sensibus.</p>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+<?php wp_footer(); ?>
+
+<!-- typing text -->
+<script>
+  var typed = new Typed('.element', {
+    strings: ["Delicious Food", "Nice Ambient", "Affordable Prices"],
+    startDelay: 10,
+    loop: true,
+    backDelay: 2000,
+    typeSpeed: 50
+  });
+</script>
+
+<!-- Video Header -->
+<script>
+  HeaderVideo.init({
+    container: $('.header-video'),
+    header: $('.header-video--media'),
+    videoTrigger: $("#video-trigger"),
+    autoPlayVideo: true
+  });
+</script>
+
+<!-- kenburns slider -->
+<script>
+  $(function() {
+    "use strict";
+    $('.kenburns_slider').vegas({
+      slides: [{
+          src: 'img/flex_slides/slide_3.jpg'
+        },
+        {
+          src: 'img/flex_slides/slide_1.jpg'
+        },
+        {
+          src: 'img/slides/slide_home_2.jpg'
+        }
+      ],
+      overlay: false,
+      transition: 'fade2',
+      animation: 'kenburnsUpRight',
+      transitionDuration: 1000,
+      delay: 4000,
+      animationDuration: 70000
+    });
+  });
+</script>
+
+</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Marinate The Restaurant functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Marinate_The_Restaurant
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function marinate_the_restaurant_setup() {
+	/*
+		* Make theme available for translation.
+		* Translations can be filed in the /languages/ directory.
+		* If you're building a theme based on Marinate The Restaurant, use a find and replace
+		* to change 'marinate-the-restaurant' to the name of your theme in all the template files.
+		*/
+	load_theme_textdomain( 'marinate-the-restaurant', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+		* Let WordPress manage the document title.
+		* By adding theme support, we declare that this theme does not use a
+		* hard-coded <title> tag in the document head, and expect WordPress to
+		* provide it for us.
+		*/
+	add_theme_support( 'title-tag' );
+
+	/*
+		* Enable support for Post Thumbnails on posts and pages.
+		*
+		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		*/
+	add_theme_support( 'post-thumbnails' );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(
+		array(
+			'menu-1' => esc_html__( 'Primary', 'marinate-the-restaurant' ),
+		)
+	);
+
+	/*
+		* Switch default core markup for search form, comment form, and comments
+		* to output valid HTML5.
+		*/
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support(
+		'custom-background',
+		apply_filters(
+			'marinate_the_restaurant_custom_background_args',
+			array(
+				'default-color' => 'ffffff',
+				'default-image' => '',
+			)
+		)
+	);
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	/**
+	 * Add support for core custom logo.
+	 *
+	 * @link https://codex.wordpress.org/Theme_Logo
+	 */
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		)
+	);
+}
+add_action( 'after_setup_theme', 'marinate_the_restaurant_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function marinate_the_restaurant_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'marinate_the_restaurant_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'marinate_the_restaurant_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function marinate_the_restaurant_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'marinate-the-restaurant' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'marinate-the-restaurant' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'marinate_the_restaurant_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function marinate_the_restaurant_scripts() {
+	wp_enqueue_style( 'marinate-the-restaurant-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'vendors', get_template_directory_uri() . '/assets/css/vendors.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/css/style.css', array(), _S_VERSION );
+	//index2.html
+  wp_enqueue_style( 'flex slider', get_template_directory_uri() . '/assets/css/flex_slider.css', array(), _S_VERSION );
+	wp_enqueue_style( 'wizard', get_template_directory_uri() . '/assets/css/wizard.css', array(), _S_VERSION );
+	wp_enqueue_style( 'vegas', get_template_directory_uri() . '/assets/css/vegas.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css', array(), _S_VERSION );
+
+	wp_enqueue_script( 'common_scripts', get_template_directory_uri() . '/assets/js/common_scripts.min.js', array(), _S_VERSION, true );
+	//index2.html
+  wp_enqueue_script( 'jquery-flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'slider', get_template_directory_uri() . '/assets/js/slider.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'slider-2', get_template_directory_uri() . '/assets/js/slider_2.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'common_func', get_template_directory_uri() . '/assets/js/common_func.js', array(), _S_VERSION, true );
+	//index3.hmtl
+  wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr.min.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'video-header', get_template_directory_uri() . '/assets/js/video_header.min.js', array(), _S_VERSION, true );
+
+  //index5.html
+	wp_enqueue_script( 'typed', get_template_directory_uri() . '/assets/js/typed.min.js', array(), _S_VERSION, true );
+
+  //SPECIFIC SCRIPTS (wizard form)
+  wp_enqueue_script( 'wizard_script', get_template_directory_uri() . '/assets/js/wizard/wizard_scripts.min.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'wizard_func', get_template_directory_uri() . '/assets/js/wizard/wizard_func.js', array(), _S_VERSION, true );
+
+
+
+
+  // wp_style_add_data( 'marinate-the-restaurant-style', 'rtl', 'replace' );
+
+	// wp_enqueue_script( 'marinate-the-restaurant-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'marinate_the_restaurant_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
+
+
+/**
+ * Get the assets folder path using `get_template_directory_uri`.
+ *
+ * @param string $path Path or name of the asset.
+ *
+ * @return string Returns the full path of the asset.
+ */
+function asset_path($path, $echo = false)
+{
+	if (!$path) {
+		return '';
+	}
+
+	$asset_url = '';
+
+	$base_path = get_template_directory_uri() . '/assets';
+
+	$asset_url = $path[0] === '/'
+		? $base_path . $path
+		: $base_path . '/' . $path;
+
+	if (!$echo) {
+		return $asset_url;
+	}
+
+	echo $asset_url;
+}
